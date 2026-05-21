1c809327f0a79fa650dd136473d526ede4f20618
ohy-zhangzl <3190105448@zju.edu.cn>
2026-05-21 17:21:33 +0800

    update rust notes day 10

--- static/rust/searcher-c2a407aa.js
+++ static/rust/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d28f18ac.js',
+                path_to_root + 'searchindex-bd04c544.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
